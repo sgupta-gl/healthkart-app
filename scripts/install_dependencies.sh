7647c96ff252a866430cfce12aab77c555fd7b58
Sanjay Gupta <Gupta.Gupta@penske.com>
2025-04-10 15:44:22 +0530

    fgjd

---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# This script will install Docker on the EC2 instance
+sudo yum update -y
+sudo yum install -y docker
+sudo service docker start
+sudo usermod -a -G docker ec2-user  # Add ec2-user to docker group to run Docker without sudo
+cd /home/ec2-user/app
+npm install express
