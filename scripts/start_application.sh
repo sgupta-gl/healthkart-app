a9d559ed2ca9d687c8d8afd56cde99590aa5d656
Sanjay Gupta <Gupta.Gupta@penske.com>
2025-04-10 17:18:38 +0530

    appspec updated

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # This script builds and starts the Docker container with your app
 cd /home/ec2-user/app
-docker build -t node-app .
 # Remove any previous container (if it exists) to avoid conflicts
 docker rm -f node-app
+docker build -t node-app .
 docker run -d -p 3001:3001 --name node-app node-app
