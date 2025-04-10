a79e06bb5585f8289fa0c6664cc8d2ea25ca512d
sgupta-gl <156300186+sgupta-gl@users.noreply.github.com>
2025-04-10 15:04:43 +0530

    Update run_docker.sh

--- scripts/run_docker.sh
+++ scripts/run_docker.sh
@@ -10,14 +10,14 @@ fi
 
 # Pull the latest Docker image from ECR
 echo "Logging into ECR..."
-aws ecr get-login-password --region eu-north-1 | docker login --username AWS --password-stdin 311141543335.dkr.ecr.eu-north-1.amazonaws.com
+aws ecr get-login-password --region eu-north-1 | docker login --username AWS --password-stdin 913524931303.dkr.ecr.eu-north-1.amazonaws.com
 
 # Pull the image
 echo "Pulling the Docker image..."
-docker pull 311141543335.dkr.ecr.eu-north-1.amazonaws.com/simple-docker-service-0a424f13d5cd:latest
+docker pull 913524931303.dkr.ecr.eu-north-1.amazonaws.com/simple-docker-service-0a424f13d5cd:latest
 
 # Run the Docker container (if it's not already running)
 echo "Running the Docker container..."
-docker run -d -p 80:80 --name healthkart-app 311141543335.dkr.ecr.eu-north-1.amazonaws.com/simple-docker-service-0a424f13d5cd:latest
+docker run -d -p 80:80 --name healthkart-app 913524931303.dkr.ecr.eu-north-1.amazonaws.com/simple-docker-service-0a424f13d5cd:latest
 
 echo "Docker container is running!"
