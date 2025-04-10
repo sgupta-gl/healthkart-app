50a8df0e098dda24201daf49a51372390708647c
Sanjay Gupta <Gupta.Gupta@penske.com>
2025-04-10 15:39:49 +0530

    added

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const fs = require("fs");
 const path = require("path");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 // Serve the health data JSON file
 app.get("/health-insights", (req, res) => {
