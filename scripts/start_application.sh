42d5d03aaf5755ce8d92b53013a3e1ab8edb35e2
Sanjay Gupta <Gupta.Gupta@penske.com>
2025-04-10 15:49:47 +0530

    added npm start

---
+++ scripts/start_application.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# This script builds and starts the Docker container with your app
+cd /home/ec2-user/app
+docker build -t node-app .
+docker run -d -p 3001:3001 --name node-app node-app
+npm start
